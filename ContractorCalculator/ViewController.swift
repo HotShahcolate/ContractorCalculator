9c6346069498c1987ce2777217f9786d33fa0525
Shah Mirza <shahjmirza@gmail.com>
2022-03-31 17:00:24 -0400

    initial

--- ContractorCalculator/ViewController.swift
+++ ContractorCalculator/ViewController.swift
@@ -8,7 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labor: UITextField!
+    @IBOutlet weak var materials: UITextField!
+    
+    @IBOutlet weak var subtotal: UILabel!
+    @IBOutlet weak var tax: UILabel!
+    @IBOutlet weak var total: UILabel!
+    
+    
+    @IBAction func calculate(_ sender: Any) {
+        let laborText = labor.text
+        let materialsText = materials.text
+        
+        guard let laborCost = Double(laborText!) else { return subtotal.text = "Error: labor must be a number" }
+        guard let masterialsCost = Double(materialsText!) else { return subtotal.text = "Error: materials must be a number" }
+        
+        let subtotalAmount = laborCost + masterialsCost
+        let subtotalString = String(subtotalAmount)
+        
+        let taxAmount = subtotalAmount * 0.05
+        let taxAmountString = String(taxAmount)
+        
+        let totalAmount = subtotalAmount + taxAmount
+        let totalAmountString = String(totalAmount)
+        
+        subtotal.text = "$" + subtotalString
+        tax.text = "$" + taxAmountString
+        total.text = "$" + totalAmountString
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
